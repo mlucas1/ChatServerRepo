c1a72fe43894533a403bb5282fd7e14de11964ec
mlucas1 <mlucas1@hwemail.com>
2013-09-09 14:40:46 -0700

    Stubs of ChatServer and ChatClient

---
+++ Chat Server Project/src/ChatServer.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author mlucas1
+ *
+ */
+public class ChatServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
